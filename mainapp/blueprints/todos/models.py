3d1d7c9a81651d123594bcc289cea8e71c7a1e39
Name-san <eman.jandog@gmai.com>
2025-06-07 14:01:55 +0800

    add completed date and fix it on model, schem, route

--- mainapp/blueprints/todos/models.py
+++ mainapp/blueprints/todos/models.py
@@ -8,7 +8,7 @@ class Todo(db.Model):
     text = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Boolean, default=False)
     creation_date = db.Column(db.Date, nullable=False, default=db.func.current_date())
-    completed_date = db.Column(db.Date, nullable=True)
+    completed_date = db.Column(db.DateTime, nullable=True)
 
     def __repr__(self):
         return f'Todo(tid={self.tid}, text={self.text})'
