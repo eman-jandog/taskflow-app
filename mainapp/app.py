90d95f8c2ec41537437fa987e6a0525b8c713d45
Eman <116855486+eman-jandog@users.noreply.github.com>
2025-09-06 15:51:59 +0800

    Update app.py
    Add: default value in environment

--- mainapp/app.py
+++ mainapp/app.py
@@ -12,7 +12,7 @@ db = SQLAlchemy()
 def create_app():
     load_dotenv()
     app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('POSTGRES_DB')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('POSTGRES_DB', 'SQLITE_DB')
     app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
     
 
@@ -40,5 +40,6 @@ def create_app():
     
     
     migrate = Migrate(app, db)
+
 
     return app
